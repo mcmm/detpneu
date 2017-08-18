bd2d26f2d1cd6a7e9497fa9bb8721d7f599865f3
Maria Carla <mariacarlade@gmail.com>
2017-08-18 10:55:57 -0300

    Melhorias
    Código com comentários e tartamento da imagem para aplicar hough

--- detpneu.cpp
+++ detpneu.cpp
@@ -3,6 +3,8 @@
 
 
 //Função canny utilizada para detectar arestas
+	//código da apostila "corner.pdf"
+	//última página
 Mat_<GRY> canny(Mat_<GRY> a)
 { Mat_<GRY> b;
  Canny(a, b, 160, 70);
@@ -11,36 +13,45 @@ Mat_<GRY> canny(Mat_<GRY> a)
 
 int main() {
 
+	//Para capturar quadros (referências):
+		//apostila "video.pdf"
+			//códigos: "leimp.cpp" (pos-2014), "levideo.cpp" (pos-2013)
+ 		//http://answers.opencv.org/question/5768/how-can-i-get-one-single-frame-from-a-video-file/
+
 	//Lê vídeo desejado em vi
  VideoCapture vi("teste.avi");
  if (!vi.isOpened()) erro("Erro abertura video entrada");
 
- //Link de referência:
- //http://answers.opencv.org/question/5768/how-can-i-get-one-single-frame-from-a-video-file/
-
  int total_frames = vi.get(CV_CAP_PROP_FRAME_COUNT); //numero total de quadros
- //int step = vi.get(CV_CAP_PROP_FRAME_COUNT)/total_frames; //distancia necessaria para pegar 10 quadros
- int num_frames = 0;
+ int num_frames = 0; //variavel que guarda número de quadros
  
- 
- 	for(int aux = 0; aux < total_frames; aux+=150){ //deseja-se pegar 10 quadros
- 		 //cria  matriz colorida para salvar quadros
+ 	for(int aux = 0; aux < total_frames; aux+=150){ //deseja-se percorrer todas as frames, 
+ 													//adiciona-se 150 para pegar mais ou menos 10 quadros
+ 		 //cria  matriz para salvar quadros
  		Mat_<GRY> frame;
  		vi.set(CV_CAP_PROP_POS_FRAMES, aux); //VideoCapture::set set the index to the next frame to be captured
- 		vi >> frame; // pega o proximo quadro do video
- 		
-
+ 		vi >> frame; // pega o proximo quadro do video - de indice definido no set
+ 	
  		//Para pegar o índice do quadro e salvar no nome da imagem
  		std::stringstream ss;
 		ss << num_frames;
 		std::string i = ss.str();
-		std::string label = "gera_quad"+i+".jpg"; //concatena strings para salvar o nome certo
-		std::string label_canny = "canny"+i+".jpg"; //concatena strings para salvar o nome certo
+		std::string label = "gera_quad"+i+".png"; //concatena strings para salvar o nome certo
+		std::string label_canny = "canny"+i+".png"; //concatena strings para salvar o nome certo
  		
  		imp(frame, label); //salva os quadros gerados
  		
  		//Chama a função canny para detectar arestas
  		Mat_<GRY> canny_frame=canny(frame);
+
+ 		//Faz o negativo da imagem canny antes de salvar - necessário para aplicar a transformada de Hough
+ 		//código da apostila "basico.pdf"
+ 		 // inv_cek.cpp grad2015
+ 		for (int l=0; l<canny_frame.rows; l++)
+ 			for (int c=0; c<canny_frame.cols; c++)
+ 				if (canny_frame(l,c)==0) canny_frame(l,c)=255;
+ 				else canny_frame(l,c)=0;
+
  		imp(canny_frame, label_canny);
 
  		num_frames +=1;
